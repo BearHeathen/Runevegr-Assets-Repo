1c568e1e25868f1838ca30f3287193caab7aa157
Gloomshroud <brandondstewart@gmail.com>
2017-05-21 21:20:28 -0500

    Added ModRecipes

--- src/main/java/net/gloomshroud/runevegr/recipe/ModRecipes.java
+++ src/main/java/net/gloomshroud/runevegr/recipe/ModRecipes.java
@@ -2,6 +2,7 @@ package net.gloomshroud.runevegr.recipe;
 
 import net.gloomshroud.runevegr.block.ModBlocks;
 import net.gloomshroud.runevegr.item.ModItems;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
@@ -9,7 +10,10 @@ public class ModRecipes {
 	
 	public static void init(){
 		
+		
+		GameRegistry.addShapelessRecipe(new ItemStack(ModItems.barleySeed),  new ItemStack(ModItems.barley));
 		// 1 Mithril ore = 1 Mithril ingot
 		GameRegistry.addSmelting(ModBlocks.oreMithril, new ItemStack(ModItems.ingotMithril), 0.7f);
+		GameRegistry.addShapedRecipe(new ItemStack(Items.BREAD), "   ", "   ", "BBB", 'B', ModItems.barley);
 	}
 }
